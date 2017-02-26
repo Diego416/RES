fb08f36bfc9844f74dc4b47206e141aa060b39c4
Diego <godie416@hotmail.com>
2017-02-26 12:39:55 -0500

    Agregando filtración por id

--- RES/Controllers/RegistrosController.cs
+++ RES/Controllers/RegistrosController.cs
@@ -18,7 +18,42 @@ namespace ConsoleApplication.Controllers
 
         public IActionResult Index()
         {
+            ViewBag.HtmlStr = "";
+            using (var context = new DBContext())
+            {
+                foreach (var entrada in context.Entradas)
+                {
+                    ViewBag.HtmlStr +="<tr><td>"+entrada.entradasId+
+                                    "</td><td>"+entrada.Ent_Ingreso+
+                                    "</td><td>"+entrada.Ent_MotivoVisita+
+                                    "</td><td>"+entrada.Ent_IDVisitante+
+                                    "</td><td>"+entrada.Ent_NombreVisitante+
+                                    "</td><td>"+entrada.Ent_ApellidoVisitante+"</td></tr>";
+                }
+            }
+            
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Filtrar(string txtID)
+        {
+            ViewBag.HtmlStr = "";
+            using (var context = new DBContext())
+            {
+                foreach (var entrada in context.Entradas)
+                {
+                    if(entrada.Ent_IDVisitante == Convert.ToInt32(txtID))
+                        ViewBag.HtmlStr +="<tr><td>"+entrada.entradasId+
+                                        "</td><td>"+entrada.Ent_Ingreso+
+                                        "</td><td>"+entrada.Ent_MotivoVisita+
+                                        "</td><td>"+entrada.Ent_IDVisitante+
+                                        "</td><td>"+entrada.Ent_NombreVisitante+
+                                        "</td><td>"+entrada.Ent_ApellidoVisitante+"</td></tr>";
+                }
+            }
+
+            return View("~/Views/Registros/Index.cshtml");
         }
     }
 }
